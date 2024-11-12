bd7ff323b916b3f510bb247efc78d2b07d6b3c96
Giulio <g@lsd.cat>
2024-11-12 17:11:24 +0100

    Submission server from lua to flask, to run in lambda. Schema updates with the new policy syntax

---
+++ submission_server/main.py
@@ -0,0 +1,122 @@
+import os
+from enum import StrEnum
+import pymysql
+from flask import Flask, jsonify, request, abort
+from werkzeug.exceptions import MethodNotAllowed, UnsupportedMediaType, HTTPException
+
+app = Flask(__name__)
+
+class ActionTypeValue(StrEnum):
+    ADD = "ADD"
+    MODIFY = "MODIFY"
+    DELETE = "DELETE"
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+def get_db_connection():
+    return pymysql.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+@app.route('/submission', methods=['POST'])
+def post_submission():
+    data = request.get_json()
+    
+    if not all(key in data for key in ("fqdn", "action")):
+        return jsonify({"status": "KO", "Error": "Missing the `fqdn` or `action` keys."}), 400
+    
+    fqdn_input = data["fqdn"]
+    type_input = data["action"].upper()
+
+    if type_input not in ActionTypeValue.__members__.values():
+        return jsonify({"status": "KO", "Error": "`action` value is not in [`ADD`, `MODIFY`, `DELETE`]"}), 400
+
+    try:
+        conn = get_db_connection()
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO submissions (submitted_fqdn, type) VALUES (%s, %s);",
+                (fqdn_input, type_input)
+            )
+            conn.commit()
+            submission_id = cursor.lastrowid
+        return jsonify({"status": "OK", "id": submission_id}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"status": "KO", "message": "A server error as occurred, contact and administrator."}), 500
+    finally:
+        conn.close()
+
+@app.route('/submission', methods=['GET'])
+def get_total_submissions():
+    try:
+        conn = get_db_connection()
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(id) as total_submissions FROM submissions;")
+            result = cursor.fetchone()
+        return jsonify({"status": "OK", "total_submissions": result["total_submissions"]}), 200
+    except Exception as e:
+        return jsonify({"status": "KO", "message": "A server error as occurred, contact and administrator."}), 500
+    finally:
+        conn.close()
+
+@app.route('/submission/<int:submission_id>', methods=['GET'])
+def get_submission(submission_id):
+    try:
+        conn = get_db_connection()
+        with conn.cursor() as cursor:
+            # Fetch submission details
+            cursor.execute("""
+                SELECT id, submitted_fqdn, fqdn, type, timestamp, status_id, status_timestamp,
+                (SELECT value FROM statuses WHERE id = submissions.status_id) AS status_value
+                FROM submissions WHERE id = %s
+            """, (submission_id,))
+            submission = cursor.fetchone()
+            
+            if not submission:
+                return jsonify({"status": "KO", "Error": "Submission with ths id provided not found."}), 404
+
+            cursor.execute("""
+                SELECT status_id, timestamp FROM status_changes WHERE submission_id = %s
+            """, (submission_id,))
+            status_log = cursor.fetchall()
+
+        return jsonify({
+            "status": "OK",
+            "id": submission["id"],
+            "fqdn": submission["fqdn"],
+            "type": submission["type"],
+            "status": submission["status_value"],
+            "log": status_log
+        }), 200
+    except Exception as e:
+        return jsonify({"status": "KO", "message": "A server error as occurred, contact and administrator."}), 500
+    finally:
+        conn.close()
+
+@app.errorhandler(MethodNotAllowed)
+def method_not_allowed(e):
+    return jsonify({"status": "KO", "error": "Method not allowed."}), 405
+
+@app.errorhandler(UnsupportedMediaType)
+def handle_unsupported_media_type(e):
+    return jsonify({"status": "KO", "error": "Unsupported media type"}), 415
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(e):
+    return jsonify({"status": "KO", "error": "An HTTP exception has occurred, check the response code."}), e.code
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return jsonify({"status": "KO", "error": "An unexpected error occurred"}), 500
+
+if __name__ == "__main__":
+    app.run()
--- submission_server/schema.sql
+++ submission_server/schema.sql
@@ -1,79 +1,119 @@
---GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO webcat;
---GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO webcat;
+CREATE DATABASE list;
+USE list;
 
-
-CREATE TABLE types (
-    id SERIAL PRIMARY KEY NOT NULL,
-    value VARCHAR NOT NULL,
-    description VARCHAR
-);
-
-CREATE TABLE submissions (
-    id SERIAL NOT NULL PRIMARY KEY,
-    submitted_fqdn VARCHAR NOT NULL,
-    fqdn VARCHAR,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    policy VARCHAR,
-    type_id SMALLINT NOT NULL REFERENCES types (id),
-    status_id SMALLINT DEFAULT 1,
-    status_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO types (value, description) VALUES ('ADD', 'New preload list submission for previously not enrolled domain.');
-INSERT INTO types (value, description) VALUES ('DELETE', 'Domain removal from preload list of previously enrolled domain.');
-INSERT INTO types (value, description) VALUES ('MODIFY', 'Policy change of already enrolled domain.');
-
+-- The content of this database is not secret. The submission API returns some values of it
+-- and ultimately every info here that leads to a change in the list is in the transparency log too.
+-- The integrity of this database is important though: foul play here is an attack, for instance
+-- it could lead to skipping certain verification phases. It is possible to detect it afterwards,
+-- but still it is best if it does not happen :)
 CREATE TABLE statuses (
-    id SERIAL PRIMARY KEY NOT NULL,
-    value VARCHAR NOT NULL,
-    description VARCHAR,
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    value VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
     completed BOOLEAN
 );
 
--- We expect SUBMITTED to be 1 since it is set as default in submissions
+-- For a detailed explanation and the verification procedure see the documentation
 INSERT INTO statuses (value, description, completed) VALUES ('SUBMITTED', 'Submission accepted in the queue.', false);
 INSERT INTO statuses (value, description, completed) VALUES ('PRELIMINARY_VALIDATION_IN_PROGRESS', 'Preliminary validation in progress.', false);
 INSERT INTO statuses (value, description, completed) VALUES ('PRELIMINARY_VALIDATION_ERROR', 'Preliminary validation failed.', true);
 INSERT INTO statuses (value, description, completed) VALUES ('PRELIMINARY_VALIDATION_OK', 'Preliminary validation succeeded.', false);
 INSERT INTO statuses (value, description, completed) VALUES ('SUBMISSION_TO_LOG_IN_PROGRESS', 'Transparency Log submission in progress.', false);
 INSERT INTO statuses (value, description, completed) VALUES ('SUBMISSION_TO_LOG_ERROR', 'Transparency Log submission failed.', true);
-INSERT INTO statuses (value, description, completed) VALUES ('SUBMISSION_TO_LOG_OK', 'Transparency Log submission succeded.', false);
+INSERT INTO statuses (value, description, completed) VALUES ('SUBMISSION_TO_LOG_OK', 'Transparency Log submission succeeded.', false);
 INSERT INTO statuses (value, description, completed) VALUES ('LOG_INCLUSION_OK', 'First inclusion proof received from Transparency Log', false);
---INSERT INTO statuses (value, description, completed) VALUES ('WAITING_DELAY', 'Waiting for the set delay before sending again to the Transparency Log.', 0);
---INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_IN_PROGRESS', 'Second Transprency Log submission in progress.', 0);
---INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_ERROR', 'Second Transparency Log submission failed.', 1);
---INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_OK', 'Second Transparency Log submission succeeded.', 0);
---INSERT INTO statuses (value, description, completed) VALUES ('WAITING_FOR_SECOND_PROOF', 'Waiting fot the entry to be inserted into the Transparency Log.', 0);
-INSERT INTO statuses (value, description, completed) VALUES ('COMPLETED', 'Procedure succesfully completed. The Preload list has been updated.', true);
+INSERT INTO statuses (value, description, completed) VALUES ('WAITING_DELAY', 'Waiting for the set delay before sending again to the Transparency Log.', false);
+INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_IN_PROGRESS', 'Second Transparency Log submission in progress.', false);
+INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_ERROR', 'Second Transparency Log submission failed.', true);
+INSERT INTO statuses (value, description, completed) VALUES ('SECOND_SUBMISSION_TO_LOG_OK', 'Second Transparency Log submission succeeded.', false);
+INSERT INTO statuses (value, description, completed) VALUES ('WAITING_FOR_SECOND_PROOF', 'Waiting for the entry to be inserted into the Transparency Log.', false);
+INSERT INTO statuses (value, description, completed) VALUES ('COMPLETED', 'Procedure successfully completed. The Preload list has been updated.', true);
 
-CREATE TABLE status_changes (
-    id SERIAL NOT NULL PRIMARY KEY,
-    submission_id INTEGER NOT NULL REFERENCES submissions (id),
-    status_id SMALLINT NOT NULL REFERENCES statuses (id),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE submissions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    -- Value stored raw, previous any normalization
+    submitted_fqdn VARCHAR(255) NOT NULL,
+    -- Normalized value
+    fqdn VARCHAR(255),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type ENUM('ADD','MODIFY','DELETE'),
+    status_id INT DEFAULT 1,
+    status_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (status_id) REFERENCES statuses (id)
 );
 
+-- We want to log everything that happens, to help both us and administrators to debug in case
+CREATE TABLE status_changes (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    submission_id INT NOT NULL,
+    status_id INT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (submission_id) REFERENCES submissions (id),
+    FOREIGN KEY (status_id) REFERENCES statuses (id)
+);
+
+-- These errors are returned to everybody via the API
 CREATE TABLE error_log (
-    id SERIAL NOT NULL PRIMARY KEY,
-    submission_id INTEGER NOT NULL REFERENCES submissions (id),
-    status_change_id INTEGER NOT NULL REFERENCES status_changes (id),
-    error VARCHAR
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    submission_id INT NOT NULL,
+    status_change_id INT NOT NULL,
+    error VARCHAR(255),
+    FOREIGN KEY (submission_id) REFERENCES submissions (id),
+    FOREIGN KEY (status_change_id) REFERENCES status_changes (id)
 );
 
 CREATE TABLE leaves (
-    id SERIAL NOT NULL PRIMARY KEY,
-    submission_id INTEGER NOT NULL REFERENCES submissions (id),
-    leaf VARCHAR,
-    hash BYTEA,
-    inclusion_hashes BYTEA ARRAY,
-    index INTEGER
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    submission_id INT NOT NULL,
+    leaf VARCHAR(255),
+    hash BLOB,
+    inclusion_hashes BLOB,
+    `index` INT,
+    FOREIGN KEY (submission_id) REFERENCES submissions (id)
 );
 
+-- This is the final list upon which 
 CREATE TABLE list (
-    id SERIAL NOT NULL PRIMARY KEY,
-    fqdn VARCHAR UNIQUE,
-    policy VARCHAR,
-    policy_hash BYTEA,
-    leaf_id INTEGER REFERENCES leaves (id),
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    fqdn VARCHAR(255) UNIQUE,
+    policy VARCHAR(255),
+    policy_hash BLOB,
+    leaf_id INT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (leaf_id) REFERENCES leaves (id)
+);
+
+CREATE TABLE sigstore_issuers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    value VARCHAR(255) NOT NULL
+);
+
+INSERT INTO sigstore_issuers (name, value) VALUES ("google", "https://accounts.google.com");
+INSERT INTO sigstore_issuers (name, value) VALUES ("microsoft", "https://login.microsoftonline.com");
+INSERT INTO sigstore_issuers (name, value) VALUES ("github", "https://github.com/login/oauth");
+INSERT INTO sigstore_issuers (name, value) VALUES ("gitlab", "https://gitlab.com");
+
+CREATE TABLE sigstore_identities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    identity VARCHAR(255) NOT NULL,
+    issuer_id INT NOT NULL,
+    FOREIGN KEY (issuer_id) REFERENCES sigstore_issuers (id)
+);
+
+CREATE TABLE policies (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    thereshold SMALLINT NOT NULL,
+    submission_id INT,
+    list_id INT,
+    FOREIGN KEY (submission_id) REFERENCES submissions (id),
+    FOREIGN KEY (list_id) REFERENCES list (id)
+);
+
+CREATE TABLE sigstore_identities_policies (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    identity_id INT NOT NULL,
+    policy_id INT NOT NULL,
+    FOREIGN KEY (identity_id) REFERENCES sigstore_identities (id),
+    FOREIGN KEY (policy_id) REFERENCES policies (id)
 );
