b0479872f8405852d7f8ff66fa62316d7f67b0d4
Giulio <g@lsd.cat>
2024-11-23 11:51:40 +0100

    better terraform organization; draft test pages

--- deploy/infra/acm.tf
+++ deploy/infra/acm.tf
@@ -59,8 +59,61 @@ resource "aws_acm_certificate_validation" "log_api" {
 }
 
 # list.
+resource "aws_acm_certificate" "list" {
+  provider          = aws.us_east_1
+  domain_name       = "list.${var.main_domain}"
+  validation_method = "DNS"
+}
 
+resource "aws_route53_record" "list_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.list.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main_domain_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "list" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.list.arn
+  validation_record_fqdns = [for record in aws_route53_record.list_validation : record.fqdn]
+}
 
 # log.
+resource "aws_acm_certificate" "log" {
+  provider          = aws.us_east_1
+  domain_name       = "log.${var.main_domain}"
+  validation_method = "DNS"
+}
 
-# .
+resource "aws_route53_record" "log_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.log.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main_domain_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "log" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.log.arn
+  validation_record_fqdns = [for record in aws_route53_record.log_validation : record.fqdn]
+}
