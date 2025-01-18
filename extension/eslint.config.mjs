82967b42a064c608b2f5b5ec3828610c3a49b658
Giulio <giulio@freedom.press>
2025-01-18 19:33:27 +0100

    Enable warn on undefined

--- extension/eslint.config.mjs
+++ extension/eslint.config.mjs
@@ -20,7 +20,7 @@ export default [
     rules: {
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-
+      "@typescript-eslint/no-non-null-assertion": "warn",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
