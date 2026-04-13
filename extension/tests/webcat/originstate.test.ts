5f36311f41fc9c27fcab7852129ff4906382322d
giulio <66009328+lsd-cat@users.noreply.github.com>
2026-04-13 10:52:36 +0200

    test: add headers validation tests

--- extension/tests/webcat/originstate.test.ts
+++ extension/tests/webcat/originstate.test.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
