6634f95806687cc96a71c7bb9b6e7c39559845a5
Juho Forsén <juho@freedom.press>
2026-08-25 06:07:11 +0000

    Set paths earlier

--- extension/src/webcat.ts
+++ extension/src/webcat.ts
@@ -8,12 +8,12 @@ import { clearBrowserCaches } from "./webcat/utils";
 
 function start(options?: Partial<WebcatConfig>) {
   const cfg = Object.assign(structuredClone(config.default), options);
+  setIconsPath(cfg.iconsPath);
+  setPagesPath(cfg.pagesPath);
+
   const db = new WebcatDatabase(cfg);
   const requestHandler = new WebcatRequestHandler(db, cfg);
   const updater = new EnrollmentUpdater(Object.assign({ database: db }, cfg));
-
-  setIconsPath(cfg.iconsPath);
-  setPagesPath(cfg.pagesPath);
 
   requestHandler.bindAll();
   requestHandler.addEventListener(
