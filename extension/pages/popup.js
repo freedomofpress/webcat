8dd83344dde48950094aaa4a32446e37b162cd7e
Juho Forsén <juho@freedom.press>
2026-09-22 14:17:49 +0000

    Add a fix button to permission warnings

--- extension/pages/popup.js
+++ extension/pages/popup.js
@@ -1,3 +1,36 @@
+const renderers = {
+  permissions: {
+    message(missing) {
+      return browser.i18n.getMessage(
+        "WEBCAT_missingPermissions",
+        Array.from(missing).join("\n"),
+      );
+    },
+    button(missing) {
+      const b = document.createElement("button");
+      b.textContent = browser.i18n.getMessage("WEBCAT_fix");
+      b.addEventListener("click", () => {
+        const [permissions, origins] = [[], []];
+        for (const permission of missing) {
+          if (permission.includes("://") || permission === "<all_urls>") {
+            origins.push(permission);
+          } else {
+            permissions.push(permission);
+          }
+        }
+        try {
+          browser.permissions.request({ permissions, origins });
+          window.close();
+        } catch {
+          // TODO: browser.permissions.request may throw. Fall back to opening
+          // a documentation page with guidance on resolving the issue manually
+        }
+      });
+      return b;
+    },
+  },
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(location.hash.slice(1));
   const warnings = JSON.parse(params.get("warnings") ?? "{}");
@@ -5,13 +38,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const warning = document.createElement("div");
     warning.id = `warning-${id}`;
     warning.classList.add("warning");
-    warning.textContent = warnings[id].message;
-    if (warnings[id].url) {
-      const info = document.createElement("a");
-      info.textContent = "\u2139";
-      info.href = warnings[id].url;
-      info.target = "_blank";
-      warning.prepend(info);
+    warning.textContent = renderers[id].message(...warnings[id]);
+    if (renderers[id].button) {
+      const button = renderers[id].button(...warnings[id]);
+      warning.prepend(button);
     }
     document.body.prepend(warning);
   }
